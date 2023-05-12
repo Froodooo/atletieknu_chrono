787f33fd7914e8dc6f68880f30797982fc0078c4
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2023-05-12 15:32:04 +0200

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,89 @@
+function configureAdd(chrono) {
+  const addButton = document.createElement("button");
+  addButton.textContent = "Toevoegen";
+  
+  addButton.onclick = function() {
+    const tableHead = chrono.getElementsByTagName("thead")[0];
+    tableHead && chrono.removeChild(tableHead);
+    const rows = chrono.getElementsByTagName("tr");
+  
+    for (let row of rows) {
+      const athletesButton = row.children[3];
+      athletesButton && athletesButton.remove();
+  
+      const timeCell = row.children[0];
+      const spanElements = timeCell.getElementsByTagName("span");
+      for (let i = spanElements.length - 1; i >= 0; i--) {
+          spanElements[i].parentNode.removeChild(spanElements[i]);
+      }
+    }
+  
+    const tableRowsHtml = chrono.getElementsByTagName("tbody")[0].innerHTML;
+    let currentRows = localStorage.getItem("chrono") ?? "";
+    currentRows += tableRowsHtml;
+    localStorage.setItem("chrono", currentRows);
+  }  
+
+  return addButton;
+}
+
+function configureClear() {
+  const clearButton = document.createElement("button");
+  clearButton.textContent = "Legen";
+  
+  clearButton.onclick = function () {
+    localStorage.removeItem("chrono");
+  }
+
+  return clearButton;
+}
+
+function configureCopySave() {
+  const copySaveButton = document.createElement("button");
+  copySaveButton.textContent = "Kopiëren en opslaan";
+  
+  copySaveButton.onclick = function () {
+    const chrono = localStorage.getItem("chrono");
+  
+    const chronoTable = document.createElement('table');
+    chronoTable.innerHTML = chrono;
+    sort.sortTable(chronoTable);
+  
+    navigator.clipboard.writeText(chronoTable.outerHTML);
+    save.saveFile(chronoTable.outerHTML);
+  }
+
+  return copySaveButton;
+}
+
+// Main function
+function run () {
+  const chrono = document.getElementById("chronoloog_1");
+  const chronoClone = chrono.cloneNode(true);
+
+  const copySaveButton = configureCopySave();
+  chrono.parentNode.prepend(copySaveButton);
+
+  const clearButton = configureClear();
+  chrono.parentNode.prepend(clearButton);
+
+  const addButton = configureAdd(chronoClone);
+  chrono.parentNode.prepend(addButton);
+}
+
+const save = new Save();
+const sort = new Sort();
+
+if (window.performance.getEntriesByType('navigation').map((nav) => nav.type).includes('reload')) {
+  run();
+} else {
+  const target = document.querySelector('body');
+  const config = { childList: true };
+  const callback = () => {
+    if (document.getElementsByClassName('chronoloogtabel')[0] && window.location.href.indexOf("team/main") > -1) {
+      run();
+    }
+  }
+  const observer = new MutationObserver(callback);
+  observer.observe(target, config);
+}
---
+++ scripts/save.js
@@ -0,0 +1,20 @@
+// See https://www.tutorialspoint.com/how-to-create-and-save-text-file-in-javascript
+class Save {
+  saveFile = (content) => {
+    // Create element with <a> tag
+    const link = document.createElement("a");
+  
+    // Create a blog object with the file content which you want to add to the file
+    const file = new Blob([content], { type: 'text/plain' });
+  
+    // Add file content in the object URL
+    link.href = URL.createObjectURL(file);
+  
+    // Add file name
+    link.download = "chrono.html";
+  
+    // Add click event to <a> tag to save file.
+    link.click();
+    URL.revokeObjectURL(link.href);
+  }
+}
